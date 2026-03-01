719bab65b1f2241b85547b5cc2498e8f97ca29cb
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-03-01 15:35:33 +0600

    chore: removed unecessary sections from the Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/sections/hero";
 import About from "@/sections/about";
-import Players from "@/sections/players";
+
 
 import News from "@/sections/news";
 import Gallery from "@/sections/gallery";
@@ -19,10 +19,9 @@ export default function Home() {
       <div className="fixed inset-0 opacity-[0.03] bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScyNTAnIGhlaWdodD0nMjUwJz48ZmlsdGVyIGlkPSdub2lzZScgeD0nMCcgeT0nMCc+PGZlVHVyYnVsZW5jZSB0eXBlPSdmcmFjdGFsTm9pc2UnIGJhc2VGcmVxdWVuY3k9JzAuNjUnIG51bU9jdGF2ZXM9JzMnIHN0aXRjaFRpbGVzPSdzdGl0Y2gnLz48L2ZpbHRlcj48cmVjdCB3aWR0aD0nMTAwJScgaGVpZ2h0PScxMDAlJyBmaWx0ZXI9J3VybCgjbm9pc2UpJyBvcGFjaXR5PScxJy8+PC9zdmc+')] -z-10" />
       <Hero />
       <About />
-      <Players />
 
-      <News />
       <Gallery />
+      <News />
       <Testimonials />
       <Sponsors />
       <CallToAction />
