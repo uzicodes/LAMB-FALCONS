dd00501c6981e4ce4f9020a285d28ea24771f5b0
Utsho Heaven Chowdhury <utsho8chowdhury@gmail.com>
2026-08-17 21:44:06 +0600

    refactor: updated Next.js configuration (next.config) to include the strict HTTP security headers. - X-Frame-Options: DENY. - X-Content-Type-Options: nosniff. - Referrer-Policy: strict-origin-when-cross-origin. - Origin-Opener-Policy: same-origin. - Content-Security-Policy (CSP).

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.com https://*.clerk.com;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://clerk.com https://*.clerk.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    connect-src 'self' https://clerk.com https://*.clerk.com;
+    worker-src 'self' blob:;
+`
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -7,6 +23,35 @@ const nextConfig = {
         hostname: "img.clerk.com",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
   },
 };
 
